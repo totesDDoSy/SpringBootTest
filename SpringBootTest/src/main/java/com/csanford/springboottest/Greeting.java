df08f46bda3542b6064da2f1ac48b62b67198181
totesDDoSy <cody.b.sanford@gmail.com>
2017-03-31 21:53:03 -0600

    Copying over (pretty much) the tutorial code.

---
+++ SpringBootTest/src/main/java/com/csanford/springboottest/Greeting.java
@@ -0,0 +1,28 @@
+package com.csanford.springboottest;
+
+/**
+ *
+ * @author Cody Sanford <cody.b.sanford@gmail.com>
+ */
+public class Greeting
+{
+
+    private final long id;
+    private final String content;
+
+    public Greeting( long id, String content )
+    {
+	this.id = id;
+	this.content = content;
+    }
+
+    public long getId()
+    {
+	return id;
+    }
+
+    public String getContent()
+    {
+	return content;
+    }
+}
